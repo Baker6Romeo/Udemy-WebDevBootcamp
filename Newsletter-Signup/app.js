98450e140763ab405e1ec08d7c80c61dfab49c2e
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-08-15 06:49:53 -0700

    Index get, public static images & css

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -6,6 +6,12 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+})
+
 app.listen(3000, function() {
   console.log("Listening on port 3000.");
 })
