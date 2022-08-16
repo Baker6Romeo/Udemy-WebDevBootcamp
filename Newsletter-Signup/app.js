8f98943fdab44708bab39b3b3af68ddd19fce2fd
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-08-16 08:23:21 -0700

    Prepare data for POSTing

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,29 +1,42 @@
-
-
 const bodyParser = require("body-parser");
 const express = require("express");
 const request = require("request");
 
 const app = express();
 
+const apiKey = "326217b4a63596c1845ef84fce06595d-us13";
+const audienceId = "ea084b66ec"
+const endpoint = "https://us13.api.mailchimp.com/3.0/"
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 })
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
 
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+      const firstName = req.body.fName;
+      const lastName = req.body.lName;
+      const email = req.body.email;
 
-  console.log((email));
-  console.log((firstName));
-  console.log((lastName));
-})
+      const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+              FNAME: firstName,
+              LNAME: lastName
+            }
+        }]
+      }
 
-app.listen(3000, function() {
-  console.log("Listening on port 3000.");
-})
+      const jsonData = JSON.stringify(data);
+
+        console.log((data))
+      })
+
+    app.listen(3000, function() {
+      console.log("Listening on port 3000.");
+    })
