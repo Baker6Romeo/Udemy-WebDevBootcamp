ae262bc91d4a09a96e6dfa562547ac7e9c7fdf25
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-10-27 09:54:33 -0700

    Mongo native driver, query stored fruits...didn't work.

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -7,14 +7,16 @@ const uri =
 const client = new MongoClient(uri);
 async function run() {
   try {
-    const database = client.db('fruitsDB');
-    const fruits = database.collection('fruits');
-    const insertFruits = await fruits.insertMany([
-            { name: "Apple", score: 8, review: "Great fruit"},
-            { name: "Orange", score: 6, review: "Kinda sour"},
-            { name: "Banana", score: 9, review: "Great stuff!!!"}
-         ]);
-    console.log(insertFruits);
+    const db = client.db('fruitsDB');
+    const fruits = db.collection('fruits');
+    const recalledFruits = db.collection('fruits').find({name: "Orange"});
+    // const insertFruits = await fruits.insertMany([
+    //         { name: "Apple", score: 8, review: "Great fruit"},
+    //         { name: "Orange", score: 6, review: "Kinda sour"},
+    //         { name: "Banana", score: 9, review: "Great stuff!!!"}
+    //      ]);
+    // console.log(insertFruits);
+    console.log(recalledFruits);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
