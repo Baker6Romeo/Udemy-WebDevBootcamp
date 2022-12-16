2c846f215ef12429345ba73aa6318f6a6d35fac4
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-12-16 06:49:03 -0800

    Person challenge attempt

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -5,45 +5,16 @@ const url = "mongodb://localhost:27017";
 mongoose.set("strictQuery", false);
 mongoose.connect(url + "/fruitsDB");
 
-const fruitSchema = new mongoose.Schema ({
+const personSchema = new mongoose.Schema ({
   name: String,
-  rating: Number,
-  review: String
+  age: Number
 });
 
-const Fruit = new mongoose.model("Fruit", fruitSchema);
+const Person = new mongoose.model("Person", personSchema);
 
-const fruit = new Fruit({
-  name: "Apple",
-  rating: 8,
-  review: "Great fruit"
+const person = new Person({
+  name: "John",
+  age: 37
 });
 
-fruit.save();
-
-
-
-const insertDocuments = function(db, callback) {
-
-  const collection = db.collection('fruits');
-
-  collection.insertMany([{
-      name: "Apple",
-      score: 8,
-      review: "Great fruit"
-    },
-    {
-      name: "Orange",
-      score: 6,
-      review: "Kinda sour"
-    },
-    {
-      name: "Banana",
-      score: 9,
-      review: "Great stuff!!!"
-    }
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-  });
-}
+person.save();
