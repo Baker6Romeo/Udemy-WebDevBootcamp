38baf5f0780a298c5a94c030daa533e8f1ba35d9
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-12-16 07:01:49 -0800

    Mongoose insertMany

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -5,6 +5,14 @@ const url = "mongodb://localhost:27017";
 mongoose.set("strictQuery", false);
 mongoose.connect(url + "/fruitsDB");
 
+const fruitSchema = new mongoose.Schema ({
+  name: String,
+  rating: Number,
+  review: String
+});
+
+const Fruit = new mongoose.model("Fruit", fruitSchema);
+
 const personSchema = new mongoose.Schema ({
   name: String,
   age: Number
@@ -17,4 +25,30 @@ const person = new Person({
   age: 37
 });
 
-person.save();
+// person.save();
+
+const kiwi = new Fruit({
+  name: "Kiwi",
+  rating: 10,
+  review: "The best fruit!"
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  rating: 4,
+  review: "A bit sour"
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  rating: 3,
+  review: "Weird texture"
+});
+
+Fruit.insertMany([kiwi, orange, banana], (err) => {
+  if (err) {
+    cosole.log(err);
+  } else {
+    console.log("Fruits logged");
+  }
+})
